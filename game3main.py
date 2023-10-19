0ec70917e001d94efd5418d1f90a7c40530b25f1
Handel <zhandel@una.edu>
2023-10-19 10:31:04 -0500

    pseudo code for game 3 part 1

---
+++ game3main.py
@@ -0,0 +1,24 @@
+#This is the main file for Linear Games 3
+
+#UNTIL WIN
+    #Ask user for which matrix they want to alter
+    #Store selected matrix in temp variable
+    
+    #Show selected matrix
+    
+    #Ask use for which element in that matrix they want to edit
+    #Store selected element in temp variable
+    
+    #CALL ALTER FUNCTION
+    
+    #SHOW Altered Matrix
+    
+    #Loop through all matrices
+        #If all elements are 0
+            #Win!
+        #Else
+            #No win yet
+    
+    
+    
+    

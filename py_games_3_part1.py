f32b272489b63be61608e4ddc0ef51c5ba4a5529
Handel <zhandel@una.edu>
2023-10-19 10:43:23 -0500

    added pseudo code and start of matrix creation

--- py_games_3_part1.py
+++ py_games_3_part1.py
@@ -1,5 +1,18 @@
 #This is the main file for Linear Games 3
 
+#Create matrices variable that contains all if the matrices we want to work upon
+matrices = [
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 0
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 1
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 2
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 3
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 4
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 5
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 6
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 7
+[[0,0,0],[0,0,0],[0,0,0]],#MATRIX 8
+]
+
 #UNTIL WIN
     #Ask user for which matrix they want to alter
     #Store selected matrix in temp variable

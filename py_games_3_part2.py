06e7c78dea797ebf428f43fe5cf751a45933a8ff
Zachary Handel <zachary.handelzh@gmail.com>
2023-10-22 23:52:58 -0500

    part 2 with random input

---
+++ py_games_3_part2.py
@@ -0,0 +1,102 @@
+#This is the main file for Linear Games 3
+import random
+
+#Create matrices variable that contains all if the matrices we want to work upon
+matrices = [
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+]
+
+#FUNCITONS******************************************************************************
+def input_matrix(matrix):
+    for i in range(9):
+        for j in range(3):
+            for k in range(3):
+                while True:
+                    try:
+                        value = int(input(f"Enter a value for matrix[{i}][{j}][{k}]: "))
+                        if 0 <= value <= 2:
+                            matrix[i][j][k] = value
+                            break
+                        else:
+                            print("PLease input an integer between 0-2")
+                    except ValueError:
+                        print("Please input an integer between 0-2")
+
+def print_matrices(x):
+    # Define the dimensions of the blocks
+
+    for j in range(0,3):
+        print()
+        for h in range(0,3):
+            print(end=" ")
+            for i in range(0,3):
+                print(x[h][j][i], end="")
+    print()
+
+    for j in range(0,3):
+        print()
+        for h in range(3,6):
+            print(end=" ")
+            for i in range(0,3):
+                print(x[h][j][i], end="")
+    print()
+
+    for j in range(0,3):
+        print()
+        for h in range(6,9):
+            print(end=" ")
+            for i in range(0,3):
+                print(x[h][j][i], end="")
+    print()
+
+def print_matrix(matrix):
+    for row in matrices[matrix]:
+        print(' '.join(map(str, row)))
+
+
+def check_win(matrices):
+    for i in range(9):
+        for j in range(3):
+            for k in range(3):
+                if matrices[i][j][k] != 0:
+                    return False
+    return True
+
+def fill_random(matrices):
+    #Fill matrices randomly
+    # Fill each matrix with random values
+    for i in range(9):
+        matrix = [[random.randint(0, 2) for _ in range(3)] for _ in range(3)]
+        matrices.append(matrix)
+
+def fill_zeros(matrices):
+    for i in range(9):
+        matrix = [[0 for _ in range(3)] for _ in range(3)]
+        matrices.append(matrix)
+#FUNCITONS******************************************************************************
+        
+def main():
+    win = False
+    h=0
+    i=0
+    j=0
+    value_range = [0,1,2]
+
+    #ask user to fill in initial state
+    input_matrix(matrices)
+    
+    print_matrices(matrices)
+    #determine if can reach all off state
+        #If true, output number of pushes
+        #Else, say it cannot be reached
+    
+main()
+    
